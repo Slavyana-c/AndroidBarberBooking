ee664db457eeb34e808028cc45d5766e86e5ac73
Slavyana Chervenkondeva <slavyana@Slavyanas-MacBook-Pro.local>
2020-03-09 20:06:03 +0000

    Initial commit

---
+++ app/src/main/java/com/example/androidbarberbooking/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.androidbarberbooking;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.androidbarberbooking.Common.Common;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private static final int APP_REQUEST_CODE = 7117;
+    CallbackManager callbackManager = CallbackManager.Factory.create();
+
+
+    @BindView(R.id.btn_login)
+    Button btn_login;
+
+    @BindView(R.id.txt_skip)
+    TextView txt_skip;
+
+    @OnClick(R.id.btn_login)
+    public void loginUser() {
+
+        Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show();
+        // facebook login?
+    }
+
+    @OnClick(R.id.txt_skip)
+    public void skipLoginJustGoHome() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.putExtra(Common.IS_LOGIN, false);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        final String EMAIL = "email";
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(MainActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                intent.putExtra(Common.IS_LOGIN, true);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+
+        if(isLoggedIn) {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            intent.putExtra(Common.IS_LOGIN, true);
+            startActivity(intent);
+        }
+
+
+
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+    }
+
+
+
+    // hash key
+
+//    private void printKeyHash() {
+//        try {
+//            PackageInfo packageInfo = getPackageManager().getPackageInfo(
+//                    getPackageName(),
+//                    PackageManager.GET_SIGNATURES
+//            );
+//            for(Signature signature : packageInfo.signatures)
+//            {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KEYHASH", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//
+//
+//
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
