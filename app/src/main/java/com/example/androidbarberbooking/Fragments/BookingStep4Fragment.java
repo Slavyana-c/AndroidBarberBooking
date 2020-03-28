db067f3a91a8aa5e13a2c887c7df4a6bcfdf4c1a
slavyana <slavyana.c@gmail.com>
2020-03-28 21:30:09 +0000

    Work on step 4 fragment.

--- app/src/main/java/com/example/androidbarberbooking/Fragments/BookingStep4Fragment.java
+++ app/src/main/java/com/example/androidbarberbooking/Fragments/BookingStep4Fragment.java
@@ -4,14 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.example.androidbarberbooking.R;
 
+import java.text.SimpleDateFormat;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class BookingStep4Fragment extends Fragment {
+
+    SimpleDateFormat simpleDateFormat;
+    LocalBroadcastManager localBroadcastManager;
+    Unbinder unbinder;
+
+    @BindView(R.id.txt_booking_barber_text)
+    TextView txt_booking_barber_text;
+    @BindView(R.id.txt_booking_time_text)
+    TextView txt_booking_time_text;
+    @BindView(R.id.txt_salon_address)
+    TextView txt_salon_address;
+    @BindView(R.id.txt_salon_name)
+    TextView txt_salon_name;
+    @BindView(R.id.txt_salon_open_hours)
+    TextView txt_salon_open_hours;
+    @BindView(R.id.txt_salon_phone)
+    TextView txt_salon_phone;
+    @BindView(R.id.txt_salon_website)
+    TextView txt_salon_website;
+
 
     static BookingStep4Fragment instance;
 
@@ -25,6 +53,11 @@ public class BookingStep4Fragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Date format on confirm view
+        simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+        localBroadcastManager = LocalBroadcastManager.getInstance(getContext());
     }
 
     @Nullable
@@ -32,7 +65,10 @@ public class BookingStep4Fragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
-        return inflater.inflate(R.layout.fragment_booking_step_four, container, false);
+        View itemView = inflater.inflate(R.layout.fragment_booking_step_four, container, false);
+        unbinder = ButterKnife.bind(this, itemView);
+        
+        return itemView;
 
     }
 }
